57021cebaedbacc2112641cb2d37d5bd91adb584
jinWan0823 <dhksl823@gmail.com>
2025-11-11 13:35:01 +0900

    🎉 PiggyPlan 가계부 프로젝트 시작!

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Noto_Sans_KR } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const notoSansKR = Noto_Sans_KR({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PiggyPlan | 돼지저금통 가계부",
+  description: "피기플랜 | 지출관리 가계부 사이트",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="kr">
+      <body
+        className={`${notoSansKR.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
