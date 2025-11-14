f560731cc3a570aec622e46d026060c225fae750
jinWan0823 <dhksl823@gmail.com>
2025-11-14 16:39:18 +0900

    ✨ 금액 퍼센트 계산 , framer-motion 그래프 애니메이션 추가

--- app/_components/common/MoneyTable.tsx
+++ app/_components/common/MoneyTable.tsx
@@ -1,14 +1,30 @@
 import ProgressBar from "./ProgressBar";
 
 export default function MoneyTable() {
+  const arr = [
+    { menu: "취미", money: 330200 },
+    { menu: "식비", money: 400333 },
+    { menu: "여행", money: 120500 },
+    { menu: "문화", money: 40500 },
+    { menu: "기타", money: 20000 },
+  ];
+
+  const total = arr.reduce((acc, cur) => acc + cur.money, 0);
+
   return (
     <div className="rounded bg-white p-4 py-8 mt-2 shadow-sm">
       <ul className="flex flex-col gap-4">
-        <ProgressBar menu="취미" />
-        <ProgressBar menu="식비" />
-        <ProgressBar menu="여행" />
-        <ProgressBar menu="문화" />
-        <ProgressBar menu="기타" />
+        {arr.map((item, idx) => {
+          const percent = (item.money / total) * 100;
+          return (
+            <ProgressBar
+              key={idx}
+              menu={item.menu}
+              money={item.money}
+              percent={percent}
+            />
+          );
+        })}
       </ul>
     </div>
   );
